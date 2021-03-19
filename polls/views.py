525f0c6403c6d823f19d59a510d5b5bfb2646ab7
yujie.li <yujie.li@dae.org>
2021-03-19 09:58:36 +0800

    update views.py and add detail.html

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
 def index(request):
@@ -9,6 +13,19 @@ def detail(request, question_id):
     return HttpResponse("you're looking at question %s." % question_id)
 
 
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {'question': question})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+
+
 def results(request, question_id):
     response = "you're looking at the results of question %s."
     return HttpResponse(response % question_id)
@@ -16,3 +33,19 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # output = ','.join([q.question_text for q in latest_question_list])
+    return HttpResponse(template.render(context, request))
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
